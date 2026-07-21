c01736e226c88a58d97a49c116ca0fda29ee130f
Gilbert Engalan <gilbertpude2003@gmail.com>
2026-07-21 14:01:23 +0800

    day 2

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
+# whole numer
 student_count = 1000
+# decimals
+rating = 4.99
+# boolean
+is_active = True
+# string
+name = "Python Programming"
+
 print(student_count)
+
+
+# string
+course = "Python Programming"
+print(len(course))
+#specific char 
+print(course[17])
+#start:end 
+print(course[0:3])
---
+++ notes.md
@@ -0,0 +1,13 @@
+1. variable naming should meaning full
+2. use underscore to separate words not space
+3. put space arount = sign in variable
+
+
+string 
+1. used quote " " ' ' 
+2. len = length of the string  
+3. function () to use the function 
+
+
+scape sequence 
+1. 
